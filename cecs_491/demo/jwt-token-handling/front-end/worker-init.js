b7515fbd6528c263d4308a53df4da2f2811caeca
v-vong3 <v-vong3@users.noreply.github.com>
2024-04-01 16:16:52 -0700

    Additional comments

--- cecs_491/demo/jwt-token-handling/front-end/worker-init.js
+++ cecs_491/demo/jwt-token-handling/front-end/worker-init.js
@@ -1,11 +1,19 @@
+// Initialize the Web Worker and set error handlers
 
+// Using JavaScript's type coersion and evaluation behaviors 
+// to detect presence of feature in browser
+if(window.Worker) { 
+    const myWorker = new Worker("./worker.js");
 
-const myWorker = new Worker("./worker.js");
+    myWorker.onmessageerror = function (e) {
+        console.log('error from worker', e);
+    }
 
-myWorker.onmessageerror = function (e) {
-    console.log('error from worker', e);
+    myWorker.onerror = function (e) {
+        console.log("worker error", e);
+    }
+} else {
+    alert('This browser does not support Web Workers!!');
 }
 
-myWorker.onerror = function (e) {
-    console.log("worker error", e);
-}
+
--- cecs_491/demo/jwt-token-handling/front-end/worker.js
+++ cecs_491/demo/jwt-token-handling/front-end/worker.js
@@ -1,4 +1,13 @@
 
+// Code here is running on a separate background thread from the 
+// current Window
+
+// NOTE: 
+// Web Workers are not meant o manipulate the DOM directly. 
+// They are for executing non-blocking (UI) code such as
+// network requests  
+
+
 const workerToken = { value: null };
 
 onmessage = function(e) {
@@ -7,7 +16,15 @@ onmessage = function(e) {
     if(workerToken.value == null) {
         workerToken.value = e.data;
 
+        // Data is always passed as a copy/duplicate
+        // between worker thread and main thread.
+        // structuredClone() is used during message posts, thus 
+        // the supported data types is limited, which makes 
+        // designing on message format a critical design decision.
         postMessage(JSON.stringify(workerToken));
+
+        // For passing large data, use "Transferable objects" version
+        // > postMessage(message, [TransferableObj])
 
         return;
     }
@@ -15,4 +32,27 @@ onmessage = function(e) {
     postMessage('Already saved');
 }
 
-console.log('fetch available?', !!fetch)
+// Demonstrates availability of Browser APIs in the
+// WorkerGlobalScope (global executing context of worker).
+// Some API availability depends if the worker is 
+// a dedicated or shared worker
+console.log('window available?', !!window);
+console.log('self available?', !!self);
+console.log('DOM available?', !!document);
+console.log('cookies available?', !!document.cookie);
+console.log('cookieStore available?', !!cookieStore);
+console.log('sessionStorage available?', !!sessionStorage);
+console.log('localStorage available?', !!localStorage);
+console.log('indexedDB available?', !!indexedDB);
+console.log('fetch available?', !!fetch);
+console.log('XMLHttpRequest available?', !!XMLHttpRequest);
+console.log('JSON available?', !!JSON);
+console.log('setTimeout available?', !!setTimeout);
+console.log('setInterval available?', !!setInterval);
+console.log('WebSockets available?', !!WebSocket);
+console.log('Crpto available?', !!Crypto.subtle);
+
+// Avoid using eval() as it is a major security vulnerability
+console.log('eval available?', !!eval);
+
+
